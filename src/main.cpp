e1b46ab4cc96af9e7f45a3d48894845e9288b266
Niewiaro <kubuskrol@gmail.com>
2023-03-22 23:28:31 +0100

    main file working

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
+#include <Led.h>
+
+Led *blinker;
 
 void setup() {
-  // put your setup code here, to run once:
+  Led arduinoBuiltInLed(2000, LED_BUILTIN);
+  blinker= &arduinoBuiltInLed;
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  blinker->blink();
 }
